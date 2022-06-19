42ebb5a42c6d9016cf48d7a3eebceb3f8828c0d6
AllenAx91 <103393916+AllenAx91@users.noreply.github.com>
2022-06-18 20:03:23 -0400

    Module 7 Challenge

---
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,59 @@
+-- CHALLENGE 8
+
+-- Deliverable 1: The Number of Retiring Employees by Title
+
+-- PART 1
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN Titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+
+-- PART 2
+
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+SELECT * FROM unique_titles;
+
+
+-- PART 3
+
+SELECT COUNT(title) as title_count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY title_count DESC;
+
+
+-- Deliverable 2 The Employees Eligible for the Mentorship Program
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+                    e.first_name,
+                    e.last_name,
+                    e.birth_date,
+                    de.from_date,
+                    de.to_date,
+                    t.title
+
+INTO mentorship_eligibilty
+
+    FROM employees as e
+    INNER JOIN dept_emp as de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN Titles as t
+        ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+        AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
